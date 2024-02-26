a96644c91cdb5dc66379752553a07a3e28eb791a
Mojtaba Mohammadi <mhmojtaba@gmail.com>
2024-02-26 22:09:15 +0330

    add user modal

--- assets/JS/user.js
+++ assets/JS/user.js
@@ -1,5 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".collapsible");
+  var instances = M.Collapsible.init(elems, {});
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems, {});
+  const modalBack = document.querySelector("#user-modal");
+  const modal = document.querySelector(".add-user-modal-cont");
+  const addUserButton = document.querySelector("#add-user");
+
+  addUserButton.addEventListener("click", () => {
+    modal.classList.add("show");
+    modalBack.classList.remove("dis-none");
   });
+
+  modalBack.addEventListener("click", () => {
+    modal.classList.remove("show");
+    modalBack.classList.add("dis-none");
+  });
+});
